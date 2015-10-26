195315de7d0a802072503c424b6ca2cd95a45e38
Matt <mjbowden@quinnipiac.edu>
2015-10-26 21:40:47 +0000

    Add success page

--- core/views.py
+++ core/views.py
@@ -11,4 +11,6 @@ class ContactCreateView(CreateView):
   model = Contact
   template_name = "contact/contact_form.html"
   fields = ['title', 'description']
-  success_url = reverse_lazy('home')
+  success_url = reverse_lazy('success')
+class Success(TemplateView):
+   template_name = "success.html"
