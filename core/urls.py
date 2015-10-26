3992be8ab1540a94e7d8c4d6dcce8c102b2baadf
Matt <mjbowden@quinnipiac.edu>
2015-10-26 21:23:43 +0000

    Add question create view

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^$', Home.as_view(), name='home'),                 
+    url(r'^$', Home.as_view(), name='home'),  
+    url(r'^contact/create/$', ContactCreateView.as_view(), name='contact_create'),
 )
--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
+
+class ContactCreateView(CreateView):
+  model = Contact
+  template_name = "contact/contact_form.html"
+  fields = ['title', 'description']
+  success_url = reverse_lazy('home')
